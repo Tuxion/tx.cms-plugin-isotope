3de9b12ede99824cddc38e69156af40b54eb4235
Beanow <beanow@oscp.info>
2012-09-24 21:58:07 +0200

    Moved plugin to public repository.

---
+++ plugin.php
@@ -0,0 +1,5 @@
+<?php namespace plugins; if(!defined('TX')) die('No direct access.'); ?>
+
+    <script type="text/javascript" src="<?php echo $plugin; ?>jquery.isotope.min.js"></script>
+
+    <link type="text/css" href="<?php echo $plugin; ?>isotope.css" rel="stylesheet" media="screen" charset="utf-8">
